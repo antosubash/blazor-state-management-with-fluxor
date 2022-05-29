e6a5db1bde764ecf003291a0e1ef19a5ca53f460
Anto Subash <antosubash@live.com>
2022-05-29 19:56:19 +0200

    added fluxor

--- FluxorWithBlazor/Program.cs
+++ FluxorWithBlazor/Program.cs
@@ -1,11 +1,18 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using FluxorWithBlazor;
-
+using Fluxor;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddFluxor(o =>
+{
+    o.ScanAssemblies(typeof(Program).Assembly);
+    o.UseReduxDevTools(rdt =>
+      {
+          rdt.Name = "My application";
+      });
+});
 await builder.Build().RunAsync();
